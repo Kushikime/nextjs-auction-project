27f3c64ce2b2c5775191cd9d48f2c9e37bd4181f
Kushikime <demcsakerik@gmail.com>
2022-08-07 05:02:00 +0200

    fix: removed old imports

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -1,7 +1,4 @@
 import Head from 'next/head';
-import CatCard from '../components/cards/cat/CatCard';
-import { mockCatCardProps } from '../components/cards/cat/CatCard.mocks';
-import HeaderLayout from '../components/layouts/Header/HeaderLayout';
 import styles from '../styles/Home.module.css';
 import { NextPageWithLayout } from './page';
 
@@ -14,21 +11,10 @@ const Settings: NextPageWithLayout = () => {
       </Head>
       <section>
         <h1>Welcome to User Settings</h1>
-        <CatCard {...mockCatCardProps.base} />
-        <CatCard {...mockCatCardProps.base} />
-        <CatCard {...mockCatCardProps.base} />
       </section>
     </div>
   );
 };
 
-Settings.getLayout = (page) => {
-  return (
-    <>
-      <HeaderLayout />
-      {page}
-    </>
-  );
-};
 
 export default Settings;
